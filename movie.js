697a8d4f72a325ff47eb067dd4d80364f204d4c4
Mary <marykwong27@gmail.com>
2019-04-14 21:24:18 -0700

    update

--- movie.js
+++ movie.js
@@ -1,4 +1,8 @@
 // This .on("click") function will trigger the AJAX Call
+Title = "";
+posterSrc = "";
+mYear="";
+
 document.querySelector("#movieSearch").addEventListener("click", function(event) {
 
   // event.preventDefault() can be used to prevent an event's default behavior.
@@ -26,25 +30,29 @@ document.querySelector("#movieSearch").addEventListener("click", function(event)
       var Title = response.Search[i].Title;
       var posterSrc = response.Search[i].Poster;
       var mYear = response.Search[i].Year;
-      $("#added-movie > tbody").append("<tr><td>" + "<img src=" + posterSrc + " width=100px height=150px>" + "</img>" + "</td><td>" + Title + "</td><td>" +
-      mYear + "</td></tr>");
-      console.log(response)
-      console.log(Title)
 
-      $("#song").show()
+      
+      // console.log(response)
+      // console.log(Title)
 
       $.ajax({
-        url: `https://itunes.apple.com/search?term=${Title}+movie&country=us&limit=5`,
+        url: `https://itunes.apple.com/search?term=${response.Search[i].Title}+movie&country=us&limit=5`,
         method: "GET"
       }).done(function(newResponse) {
+        console.log(this)
         var newResults = JSON.parse(newResponse).results[0].previewUrl;
         var description = JSON.parse(newResponse).results[0].longDescription
-        console.log(newResults);
-        console.log(newResponse);
-        $("#added-movie > tbody").prepend("<tr><td>"+ "<video src=" + newResults + " width=320 height=200 controls preload></video>"+ "</td><td>"+ description+"</td></tr>");
+        // console.log(newResults);
+        // console.log(newResponse);
+        $("#added-movie > tbody").append("<tr><td class=eachMovie>" + "<img src=" + posterSrc + " width=100px height=150px>" + "</img>" + "</td><td>" + Title + "</td><td>" +
+        mYear + "</td></tr>");
+        $("#added-movie > tbody").append("<tr><td>"+ "<video src=" + newResults + " width=320 height=200 controls preload></video>"+ "</td><td>"+ description+"</td></tr>");
+        console.log(Title)
+
       
     
       });
+
     };
   });
 });
