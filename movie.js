990d100895e098d09f66174391d00d99c92acceb
Mary <marykwong27@gmail.com>
2019-04-18 20:52:51 -0700

    update

--- movie.js
+++ movie.js
@@ -5,18 +5,32 @@ $(function(){
   });
  }); 
 
+//display this when no movie title is found
+function displayFalse(){
+  $("#failedSearch").show();
+}
+
+//function to use to pull from itunes API
 function loadAjax(Title, posterSrc, mYear) {
   $.ajax({
     url: `https://itunes.apple.com/search?term=${Title}+movie&country=us&limit=10`,
-    method: "GET"
+    method: "GET",
   }).done(function(newResponse) {
     var newResults = JSON.parse(newResponse).results[0].previewUrl;
     var description = JSON.parse(newResponse).results[0].longDescription
-    console.log(newResults);
-    console.log(newResponse);
-    $("#added-movie > tbody").append("<tr><td>" + "<img src=" + posterSrc + " width=100px height=150px>" + "</img>" + "</td><td>" + Title + "</td><td>" +
-  mYear + "</td><td>"+ "<video src=" + newResults + " width=320 height=200 controls preload></video>"+ "</td><td>"+ description+"</td></tr>");
-  //   $("#added-movie > tbody").append("<tr><td>"+ "<video src=" + newResults + " width=320 height=200 controls preload></video>"+ "</td><td>"+ description+"</td></tr>");
+    // console.log(newResults);
+    // console.log(newResponse);
+    // console.log(description);
+    //the description is undefined, replace it
+    if (description === undefined) {
+      
+      $("#added-movie > tbody").append("<tr><td>" + "<img src=" + posterSrc + " width=100px height=150px>" + "</img>" + "</td><td>" + Title + "</td><td>" +
+      mYear + "</td><td>"+ "<video src=" + newResults + " width=320 height=200 controls preload></video>"+ "</td><td>"+ "We don't have a description available for this movie right now, but please enjoy this song associated to the movie."+"</td></tr>");
+    }//else do this
+    else {
+      $("#added-movie > tbody").append("<tr><td>" + "<img src=" + posterSrc + " width=100px height=150px>" + "</img>" + "</td><td>" + Title + "</td><td>" +
+      mYear + "</td><td>"+ "<video src=" + newResults + " width=320 height=200 controls preload></video>"+ "</td><td>"+ description+"</td></tr>");
+    }
   });
 }
 
@@ -29,30 +43,38 @@ document.querySelector("#movieSearch").addEventListener("click", function(event)
   event.preventDefault();
 
   //display table
-  $("#searchTable").show();
 
   // Here we grab the text from the input box
   var movie = document.querySelector("#searchTerm").value;
-
-  $("#searchTerm").val("");
-  // Here we construct our URL
-  var queryURL = `https://www.omdbapi.com/?s=${movie}&apikey=598a871e`;
-  const APIKey = "598a871e";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).done(function(response) {
-    // var results = response.data;
-    $(".content").empty();
-
-    for (var i = 0; i < 10; i++) {
-      var Title = response.Search[i].Title;
-      var posterSrc = response.Search[i].Poster;
-      var mYear = response.Search[i].Year;
-      loadAjax(Title, posterSrc, mYear)
-      console.log(response)
-      console.log(Title)
-
-    };
-  });
+    //get the value of the search 
+    $("#searchTerm").val("");
+    // Here we construct our URL
+    var queryURL = `https://www.omdbapi.com/?s=${movie}&apikey=598a871e`;
+    const APIKey = "598a871e";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+      }).done(function(response) {
+        //do this if the response returns false
+        if(response.Response === "False") {
+          displayFalse();
+        }
+        //else do this
+        else{
+          //show search table, empty the content on each new search and hide the failed search
+          $(".content").empty();
+          $("#failedSearch").hide();
+          $("#searchTable").show();
+          //loop through the search results
+          for (var i = 0; i < 10; i++) {
+            var Title = response.Search[i].Title;
+            var posterSrc = response.Search[i].Poster;
+            var mYear = response.Search[i].Year;
+            //function to get itunes info based on Title and display everything in one line
+            loadAjax(Title, posterSrc, mYear)
+            // console.log(response)
+            // console.log(Title)
+        };
+      }
+    });
 });
